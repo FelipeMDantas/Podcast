ef689897e893df38d4fa78844cc7b410d00f9a38
FelipeMDantas <felipemdantas@terra.com.br>
2024-06-08 18:47:44 -0300

    PocastCard component and visual assets added to the project.

---
+++ components/RightSidebar.tsx
@@ -0,0 +1,9 @@
+const RightSidebar = () => {
+  return (
+    <section className="right_sidebar text-white-1">
+      <h1 className="text-20 font-bold text-white-1">RightSidebar</h1>
+    </section>
+  );
+};
+
+export default RightSidebar;
