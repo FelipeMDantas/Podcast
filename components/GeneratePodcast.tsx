fe8c23b46e12396488ccd1c037c76b33d61a66d1
FelipeMDantas <felipemdantas@terra.com.br>
2024-06-13 23:13:40 -0300

    Convex integration with UploadStuff structured.

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -4,9 +4,18 @@ import { Textarea } from "./ui/textarea"
 import { Button } from "./ui/button"
 import { Loader } from "lucide-react"
 import { useState } from "react"
+import { useAction, useMutation } from "convex/react"
+import { api } from "@/convex/_generated/api"
+import { v4 as uuidv4} from "uuid"
+import {useUploadFiles} from '@xixixao/uploadstuff/react'
 
 const useGeneratePodcast = ({setAudio, voiceType, voicePrompt, setAudioStorageId}: GeneratePodcastProps) => {
   const [isGenerating, setIsGenerating] = useState(false)
+
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+  const {startUpload} = useUploadFiles(generateUploadUrl)
+
+  const getPodcastAudio = useAction(api.openai.generateAudioAction)
 
   const generatePodcast = async ()=> {
     setIsGenerating(true)
@@ -17,7 +26,15 @@ const useGeneratePodcast = ({setAudio, voiceType, voicePrompt, setAudioStorageId
     }
 
     try {
-      
+      const response = await getPodcastAudio({
+        voice: voiceType,
+        input: voicePrompt
+      })
+
+      const blob = new Blob([response], {type: 'audio/mpeg'});
+      const fileName = `podcast-${uuidv4()}.mp3`;
+      const file = new File([blob], fileName, {type: 'audio/mpeg'});
+
     } catch (error) {
       console.log('Error generating podcastData', error)
       setIsGenerating(false)
